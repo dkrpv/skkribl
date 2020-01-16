bbeda64fd8ffc122bc5696aa2ce5cc22ead31bd6
Senpai <danila.karpov@gmail.com>
2020-01-16 18:06:35 +0200

    Add Bitmap Resize

--- testScreenShot/Program.cs
+++ testScreenShot/Program.cs
@@ -9,18 +9,36 @@ namespace testScreenShot
 {
     class Program
     {
-        static void Main(string[] args)
+        public static Bitmap ResizeBitmap(Bitmap bmp, int width, int height)
         {
-            Image jpg = Image.FromFile("C:/Users/danil/source/repos/testScreenShot/testScreenShot/image.jpg");
-            Bitmap img = new Bitmap(jpg);
-            for (int i = 0; i < img.Width; i++)
+            Bitmap result = new Bitmap(width, height);
+            using (Graphics g = Graphics.FromImage(result))
             {
-                for (int j = 0; j < img.Height; j++)
+                g.DrawImage(bmp, 0, 0, width, height);
+            }
+
+            return result;
+        }
+
+        private static void getPixelsetPixel()
+        {
+            Image jpg = Image.FromFile("C:/Users/danila/skkribl/testScreenShot/image.jpg");
+            Bitmap img = new Bitmap(jpg);
+            Bitmap nbmp = ResizeBitmap(img, 100, 100);
+
+            for (int i = 0; i < nbmp.Width; i++)
+            {
+                for (int j = 0; j < nbmp.Height; j++)
                 {
-                    Color pixel = img.GetPixel(i, j);
+                    Color pixel = nbmp.GetPixel(i, j);
                     Console.WriteLine(pixel);
                 }
             }
+
+        }
+        static void Main(string[] args)
+        {
+            getPixelsetPixel();
         }
     }
 }
