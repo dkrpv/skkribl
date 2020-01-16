350a69e46badc02a582d0134bd1e548c0c5515e9
Senpai <danila.karpov@gmail.com>
2020-01-16 21:03:09 +0200

    Add Virtual Mouse

--- testScreenShot/Program.cs
+++ testScreenShot/Program.cs
@@ -4,11 +4,65 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows;
 
 namespace testScreenShot
 {
     class Program
     {
+        public static class VirtualMouse
+        {
+            [DllImport("user32.dll")]
+            static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
+            private const int MOUSEEVENTF_MOVE = 0x0001;
+            private const int MOUSEEVENTF_LEFTDOWN = 0x0002;
+            private const int MOUSEEVENTF_LEFTUP = 0x0004;
+            private const int MOUSEEVENTF_RIGHTDOWN = 0x0008;
+            private const int MOUSEEVENTF_RIGHTUP = 0x0010;
+            private const int MOUSEEVENTF_MIDDLEDOWN = 0x0020;
+            private const int MOUSEEVENTF_MIDDLEUP = 0x0040;
+            private const int MOUSEEVENTF_ABSOLUTE = 0x8000;
+            public static void Move(int xDelta, int yDelta)
+            {
+                mouse_event(MOUSEEVENTF_MOVE, xDelta, yDelta, 0, 0);
+            }
+            public static void MoveTo(int x, int y)
+            {
+                mouse_event(MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_MOVE, x, y, 0, 0);
+            }
+            public static void LeftClick()
+            {
+                mouse_event(MOUSEEVENTF_LEFTDOWN, System.Windows.Forms.Control.MousePosition.X, System.Windows.Forms.Control.MousePosition.Y, 0, 0);
+                mouse_event(MOUSEEVENTF_LEFTUP, System.Windows.Forms.Control.MousePosition.X, System.Windows.Forms.Control.MousePosition.Y, 0, 0);
+            }
+
+            public static void LeftDown()
+            {
+                mouse_event(MOUSEEVENTF_LEFTDOWN, System.Windows.Forms.Control.MousePosition.X, System.Windows.Forms.Control.MousePosition.Y, 0, 0);
+            }
+
+            public static void LeftUp()
+            {
+                mouse_event(MOUSEEVENTF_LEFTUP, System.Windows.Forms.Control.MousePosition.X, System.Windows.Forms.Control.MousePosition.Y, 0, 0);
+            }
+
+            public static void RightClick()
+            {
+                mouse_event(MOUSEEVENTF_RIGHTDOWN, System.Windows.Forms.Control.MousePosition.X, System.Windows.Forms.Control.MousePosition.Y, 0, 0);
+                mouse_event(MOUSEEVENTF_RIGHTUP, System.Windows.Forms.Control.MousePosition.X, System.Windows.Forms.Control.MousePosition.Y, 0, 0);
+            }
+
+            public static void RightDown()
+            {
+                mouse_event(MOUSEEVENTF_RIGHTDOWN, System.Windows.Forms.Control.MousePosition.X, System.Windows.Forms.Control.MousePosition.Y, 0, 0);
+            }
+
+            public static void RightUp()
+            {
+                mouse_event(MOUSEEVENTF_RIGHTUP, System.Windows.Forms.Control.MousePosition.X, System.Windows.Forms.Control.MousePosition.Y, 0, 0);
+            }
+        }
         public static Bitmap ResizeBitmap(Bitmap bmp, int width, int height)
         {
             Bitmap result = new Bitmap(width, height);
@@ -31,7 +85,14 @@ namespace testScreenShot
                 for (int j = 0; j < nbmp.Height; j++)
                 {
                     Color pixel = nbmp.GetPixel(i, j);
-                    Console.WriteLine(pixel);
+                }
+            }
+            for (int x = 10000; x < 24000; x+=20)
+            {
+                for (int y = 10000; y < 24000; y+=20)
+                {
+                    VirtualMouse.MoveTo(x, y);
+                    VirtualMouse.LeftClick();
                 }
             }
 
